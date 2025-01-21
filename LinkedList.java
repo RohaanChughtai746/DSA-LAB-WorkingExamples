a82e7655f18b53331533dffeebbdc4b4764fa24a
RohaanChughtai746 <rohaanmughal733@gmail.com>
2025-01-21 22:48:54 +0500

    Add files via upload

--- LinkedList.java
+++ LinkedList.java
@@ -2,57 +2,62 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package LAB1;
+package Assignments;
 
 /**
  *
  * @author HP
  */
-class Node {
-  int data;
-  Node address;  
-public Node(int data){
-this.data=data;
-this.address=null;
-}
-}
+public class LinkedList {
 
-public class LinkedList{
-    Node head,tail;
-    int size;
-    public LinkedList(){
-        head=tail=null;
-        size=0;
+    // Inner Node class
+    static class Node {
+        int data;
+        Node next;
+
+        // Constructor
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
-    public void add(int d) {
-        Node n = new Node(d);
-        if(head==null) {
-            head=tail=n;
-        }
-        else {
-            tail.address=n;
-            tail=n;
-        }
-        size++;
+
+    // Method to display the list in reverse using recursion
+    public static void displayReverse(Node head) {
+        if (head == null) 
+            return; // Base case
+        displayReverse(head.next); // Recursive call
+        System.out.print(head.data + " "); // Print data after recursion
     }
-    public void printList(){
-        Node current = head;
-        while(current != null){
-            System.out.println(current.data + " ");
-            current = current.address;
+
+    // Method to print the list normally
+    public static void printList(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " "); // Print current node's data
+            head = head.next; // Move to the next node
         }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // Create linked list nodes
+        Node a = new Node(5);
+        Node b = new Node(23);
+        Node c = new Node(44);
+        Node d = new Node(22);
+
+        // Link nodes
+        a.next = b;
+        b.next = c;
+        c.next = d;
+
+        // Print the linked list
+        System.out.print("Original List: ");
+        printList(a);
         System.out.println();
+
+        // Display the list in reverse
+        System.out.print("Reversed List: ");
+        displayReverse(a);
     }
-    public void printSize() {
-        System.out.println("Size of the linked list: "+size );
-    }
-       public static void main(String[] args) {
-          LinkedList list = new LinkedList();
-          list.add(1);
-          list.add(2);
-          list.add(3);
-          list.add(4);
-          list.printList();
-          list.printSize();
-        }   
 }
